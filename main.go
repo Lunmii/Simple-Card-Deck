e02ae3ff4135ca94e167ce20de9521b1ee5487cd
Alo Oluwapelumi David <pelumidavid123456@gmail.com>
2022-10-09 00:27:24 +0100

    commit

--- main.go
+++ main.go
@@ -3,8 +3,14 @@ package main
 import "fmt"
 
 func main() {
-	// var card string = "ace of spade
-	card := "Ace of Spades"
-	card = "Five of diamond"
-	fmt.Println(card)
+	cards := []string{"Ace Of Spade", randomCard()}
+	cards = append(cards, "Six of spades")
+
+	for i, card := range cards {
+		fmt.Println(i, card)
+	}
+}
+
+func randomCard() string {
+	return "Black Jack"
 }
